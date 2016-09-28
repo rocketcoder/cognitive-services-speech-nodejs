374c4405b6d35aed400eb0e87f8f48d705ec9b87
unknown <cjmkamcem4@hotmail.com>
2016-09-28 17:47:17 -0400

    Updated config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"key" : "9d40a9732ce44a22827fd244abbb0549",
+	"key" : "YOUR SUBSCRIPTIN KEY HERE",
 	"fileName" : "test.wav",
 	"userAgent" : "test cog app",
 	"message" : "So in everything, do to others what you would have them do to you."
